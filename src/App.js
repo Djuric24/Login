b44267795553e4a502091c2e6a01fc5cc83673d0
djuric24 <djuric24a@gmail.com>
2024-03-11 20:31:32 +0100

    login and register working now

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import React from "react";
 import { useState, useEffect } from "react";
-//umesto slike kad se ulogujem da bude counter , - broj +
-//i na klik minus i plus da radi minus i plus 1 na pocetni broj
-//da pamti za svakog korisnika koji je trenutni broj i kad se ulogujes da vrati na pocetni broj tog korisnika
-//i da sifra kad se logujem nepokaze koju kucam     x
+
 export const App = () => {
+  const [page,setPage] = useState("login")
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
-  const [registration, setRegistration] = useState({});
+  const [loggedInUser, setLoggedInUser] = useState({});
   const [acess, setAcess] = useState(false);
   const [number, setNumber] = useState(0);
 
-  useEffect(() => {
-    const storedUserName = localStorage.getItem('userName');
-    const storedPassword = localStorage.getItem('password');
-    const storedRegistration = localStorage.getItem('registration');
-
-    if(storedUserName) {
-      setUserName(storedUserName);
-    }
-    if(storedPassword) {
-      setPassword(storedPassword);
-    }
-    if(storedRegistration) {
-      setRegistration(JSON.parse(storedRegistration));
-    }
-  }, []);
+ const register = () => {
+    let newUser = {
+      username : userName,
+      password : password,
+      currentNumber : 0
+    };
+    const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) ?? [];
+    const updatedUsers = [...registeredUsers, newUser];
+    localStorage.setItem('registeredUsers', JSON.stringify(updatedUsers));
+    // setRegistration(accountObj);
+    setUserName('');
+    setPassword('');
+    // registeredUsers.push(accountObj);
+    // izvuces sve registracije iz localst i dodas zadnju registraciju i upises
+    // localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+  }
 
   const login = () => {
-    if(registration.username === userName && registration.password === password) {
-      setAcess(true);
-       localStorage.setItem('loggedInUser', JSON.stringify(registration));
-    } else if (registration.username === userName || registration.password === password) {
-      alert('niste uneli odgovaraje korisnicko ime ili lozinku');
+    const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers"))
+    console.log(registeredUsers);
+
+    registeredUsers.forEach((user) => {
+        if(user.username === userName && user.password === password) {
+          setAcess(true);
+          setLoggedInUser(user);
+        }
+    })
       setUserName('');
       setPassword('');
     }
-  }
+
   const logOut = () => {
     setAcess(false);
     setUserName('');
@@ -45,49 +48,33 @@ export const App = () => {
   if(acess) {
     return(
       <div className="container">
-        <h1>Odobren vam je pristup</h1>
+        <h1> Welcome {loggedInUser.username}</h1>
         <button className="btn" onClick={logOut}>Izloguj se</button>
         <button onClick={() => setNumber(number + 1)}>+</button>
         <h1>{number}</h1>
         <button onClick={() => setNumber(number - 1)}>-</button>
+        
       </div>
-    )
+    ) 
   }
-  const handleButtonDelete = () => {
-    setRegistration({});
-    setUserName('');
-    setPassword('');
-    return
-  }
+
 
   const userInput = (event) => {
     let loginValue = event.target.value;
     setUserName(loginValue);
     localStorage.setItem('userName',loginValue);
+    //proveri jel trebaju ova 2  lokal storidza
   }
   const passwordInput = (event) => {
     let passwordValue = event.target.value;
     setPassword(passwordValue);
     localStorage.setItem('password',passwordValue);
   }
-  const register = () => {
-    let accountObj = {
-      username : userName,
-      password : password,
-      // currentNumber : 0
-    };
-    setRegistration(accountObj);
-    setUserName('');
-    setPassword('');
-    localStorage.setItem('registration', JSON.stringify(accountObj));
-    // console.log(registration);
-    // 
-  }
+ 
 
-  if(registration.username && registration.password) {
+  if(page === "login") {
     return (
       <div className="container">
-       <h1>Cestitam. Uspesno ste se registrovali!</h1>
        <h3>Molim ulogujte se</h3>
        <div className="form-group">
           <label htmlFor="">Korisnicko ime:  </label>
@@ -98,12 +85,16 @@ export const App = () => {
           <input type="password" className="password-input" value={password} onChange={passwordInput}></input>
        </div>
        <button className="button" onClick={login}> Login </button>
-       <button className="btn" onClick={handleButtonDelete} >Register</button>
+       <button className="btn" onClick={() => setPage("register")} >Register</button>
     </div>
     )
   };
-
-  return (
+// Kad se refreshuje da zapamti podatke,da moze vise korisnika
+// pravis novu funckiju koja na svaki pritisak dugmeta upisuje novu vrednost za broj u localstorrage
+// useEfect(() => {
+    // kad se klikne na dugme da upise u localstorage
+// },[number])  
+return (
     <div className="container">
        <div className="form-group" >
         <h1>Dobro dosli, molim registrujte se </h1>
@@ -114,7 +105,11 @@ export const App = () => {
         <label htmlFor="password">Unesite vasu lozinku:  </label>
         <input type="password" className="password-input" value={password} onChange={passwordInput} />
       </div>
-      <button className="button" onClick={register}> Register </button>
+
+      <button className="button" onClick={() => {
+        register()
+        setPage("login")
+        }}> Register </button>
     </div>
   )
 
