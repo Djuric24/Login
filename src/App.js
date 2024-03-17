9f8498f42bba8f5d88727a9b3561e865deabb052
djuric24 <djuric24a@gmail.com>
2024-03-17 14:35:52 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,105 +1,110 @@
+import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
-
 export const App = () => {
+  const apiUrl = "http://localhost:3333"
   const [page,setPage] = useState("login")
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [loggedInUser, setLoggedInUser] = useState({});
   const [acess, setAcess] = useState(false);
-  const [number, setNumber] = useState(() => {
-    const storedNumber = localStorage.getItem('number');
-    return storedNumber ? parseInt(storedNumber, 10) : 0;
-  } );
+  const [number, setNumber] = useState(0);
 
- 
-//   useEffect(() => {
-//   // Pratimo promene number i ažuriramo lokalno skladište
-//   const storedUsers = JSON.parse(localStorage.getItem("registeredUsers")) ?? [];
-//   const updatedUsers = storedUsers.map(user => {
-//     if (user.username === loggedInUser.username) {
-//       return {
-//         ...user,
-//         currentNumber: number
-//       };
-//     }
-//     return user;
-//   });
-
-//   localStorage.setItem('registeredUsers', JSON.stringify(updatedUsers));
-// }, [number, loggedInUser]);
 useEffect(() => {
-  const storedUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
-  const loggedInUser = storedUsers.find(user => user.username === userName);
+  console.log(number);
+  if(loggedInUser.id) {
+    updateUserOnBackend()
+}
+},[number])
 
-  if (loggedInUser) {
-    setNumber(loggedInUser.currentNumber || 0);
-  } else {
-    // Postavi vrednost number na 0 ako trenutni korisnik nije pronađen u lokalnom skladištu
-    setNumber(0);
-  }
-}, [userName]);
-
-
-
-
- const register = () => {
+ const register =async () => {
   if(!userName || !password) {
     alert('Molim unesite Vase korisnicko ime i lozinku');
-    return;
+    return; //ovaj return ne radi?
   }
   //ako nije ispunjeno da neupisuje nista u bazu
     let newUser = {
       username : userName,
       password : password,
-      currentNumber : 0
+      number : 0
     };
-    const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) ?? [];
-    const updatedUsers = [...registeredUsers, newUser];
-    localStorage.setItem('registeredUsers', JSON.stringify(updatedUsers));
-    // setRegistration(accountObj);
+    let res = await axios.post(apiUrl+"/savedata",newUser)
+    console.log(res?.data)
     setUserName('');
     setPassword('');
+    setPage('login'); //proslo
+    //zadatak da proveri da li je uspesan register i tek onda da prebaci na log
   }
-
-  const login = () => {
-    const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers"))
-    let found = false
-    console.log(registeredUsers);
-
-    registeredUsers.forEach((user) => {
-        if(user.username === userName && user.password === password) {
-          setAcess(true);
-          found = true
-          setLoggedInUser(user);
-          setUserName('');
-          setPassword('');
-        }
-    })
-    if(found === false) {
-      alert("nije dobro")
+const updateUserOnBackend = async () => {
+  const updatedUser = {
+    id : loggedInUser.id,
+    number : number
+  }
+  let res = await axios.put(apiUrl+"/updatedata",updatedUser);
+}
+  const login =async () => {
+    const user = {
+      username : userName,
+      password : password
+    }  
+    let res = await axios.post(apiUrl+"/login",user);
+    let odgovorSaServera = res.data
+    if(odgovorSaServera.id) {
+      if(!odgovorSaServera.city) {
+        alert('nemas grad! ');
+      }
+      setLoggedInUser(odgovorSaServera);
+      setNumber(odgovorSaServera.number);
+      setAcess(true);
     }
+    else if(!odgovorSaServera.id) {
+      alert(odgovorSaServera.message);
+
     }
+  }
 
   const logOut = () => {
     setAcess(false);
     setUserName('');
     setPassword('');
   }
+  // ako nepostoji grad , ispod welcome da pise input sa labelom unesi grad
   if(acess) {
-    return(
-      <div className="container">
-        <h1> Welcome   {loggedInUser.username}</h1>
-        <button className="operation" onClick={() => {
-          setNumber(number + 1);
-          console.log('POvecano na:', number + 1);
-          }}>+</button>
-        <h1>{number}</h1>
-        <button className="operation" onClick={() => setNumber(number - 1)}>-</button>
-        <button className="btn" onClick={logOut}>Izloguj se</button>
-        
-      </div>
+    if(!odgovorSaServera.city) {
+      return (
+        <div className="container">
+          <h1> Welcome   {loggedInUser?.username}</h1>
+          <label htmlFor="">Unesi grad</label>
+          <input type="text" />
+          
+          <button className="operation" onClick={() => {
+            setNumber(number + 1);
+            console.log('POvecano na:', number + 1);
+            }}>+</button>
+          <h1>{number}</h1>
+          <button className="operation" onClick={() => setNumber(number - 1)}>-</button>
+          <button className="btn" onClick={logOut}>Izloguj se</button>
+          
+        </div>
+      )
+    } else {
+   
+      return(
+      
+        <div className="container">
+          <h1> Welcome   {loggedInUser?.username}</h1>
+          
+          <button className="operation" onClick={() => {
+            setNumber(number + 1);
+            console.log('POvecano na:', number + 1);
+            }}>+</button>
+          <h1>{number}</h1>
+          <button className="operation" onClick={() => setNumber(number - 1)}>-</button>
+          <button className="btn" onClick={logOut}>Izloguj se</button>
+          
+        </div>
     ) 
+    }
   }
 
 
