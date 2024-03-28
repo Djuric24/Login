308a57ee57adf5d38668d47525f174c73f0b99e2
djuric24 <djuric24a@gmail.com>
2024-03-28 17:23:14 +0100

    btn-city

--- src/App.js
+++ src/App.js
@@ -10,11 +10,29 @@ export const App = () => {
   const [loggedInUser, setLoggedInUser] = useState({});
   const [acess, setAcess] = useState(false);
   const [number, setNumber] = useState(0);
+  const [city, setCity] = useState('');
+  const [editCity, setEditCity] = useState(false);
   
+ const updateCity = (event) => {
+    let newCity = event.target.value;
+    setCity(newCity);
+  } 
+  const userInput = (event) => {
+    let loginValue = event.target.value;
+    setUserName(loginValue);
+  }
+  const passwordInput = (event) => {
+    let passwordValue = event.target.value;
+    setPassword(passwordValue);
+  }
+// - update city kao i number, salje city kao deo objekta
+// - dugme da se napravi za reset city.  update da radi, ali da salje undefined.
+// - dugme style da se inplementira
+// - input style isto da se implementira
+
 
 useEffect(() => {
-  console.log(number);
-  if(loggedInUser.id) {
+  if(loggedInUser.id && loggedInUser.number !== number) {
     updateUserOnBackend()
 }
 },[number])
@@ -31,7 +49,7 @@ useEffect(() => {
       number : 0
     };
     let res = await axios.post(apiUrl+"/savedata",newUser)
-    console.log(res?.data)
+    // console.log(res?.data)
     setUserName('');
     setPassword('');
     setPage('login'); //proslo 
@@ -40,9 +58,14 @@ useEffect(() => {
 const updateUserOnBackend = async () => {
   const updatedUser = {
     id : loggedInUser.id,
-    number : number
+    number : number ?? loggedInUser.number,
+    city : city ?? loggedInUser.city
   }
   let res = await axios.put(apiUrl+"/updatedata",updatedUser);
+  let noveVrednostiSaServera = res.data
+  console.log(noveVrednostiSaServera)
+  setLoggedInUser({...noveVrednostiSaServera});
+
 }
   const login =async () => {
     const user = {
@@ -52,9 +75,7 @@ const updateUserOnBackend = async () => {
     let res = await axios.post(apiUrl+"/login",user);
     let odgovorSaServera = res.data
     if(odgovorSaServera.id) {
-      if(!odgovorSaServera.city) {
-        alert('nemas grad! ');
-      }
+      
       setLoggedInUser(odgovorSaServera);
       setNumber(odgovorSaServera.number);
       setAcess(true);
@@ -70,61 +91,44 @@ const updateUserOnBackend = async () => {
     setUserName('');
     setPassword('');
   }
-  // ako nepostoji grad, ispod welcome da pise input sa labelom unesi grad
+
   if(acess) {
-    if(!loggedInUser.city) { 
-      return (
-        <div className="container">
-          <h1> Welcome   {loggedInUser?.username}</h1>
-          <label htmlFor="">Unesi grad</label>
-          <input type="text" />
-          
-          <button className="operation" onClick={() => {
-            setNumber(number + 1);
-            console.log('POvecano na:', number + 1);
-            }}>+</button>
-          <h1>{number}</h1>
-          <button className="operation" onClick={() => setNumber(number - 1)}>-</button>
-          <button className="btn" onClick={logOut}>Izloguj se</button>
-          
-        </div>
-      )
-    } else {
-   
+    
       return(
-      
         <div className="container">
           <h1> Welcome   {loggedInUser?.username}</h1>
+          <h2>{loggedInUser?.city}</h2>
           {/* {!loggedInUser.city && <div>
              <label htmlFor="">Unesi grad</label>
           <input type="text" />
             </div>} */}
+          <button className="button-primary" onClick={() => {
+            if(editCity === true) {
+              setEditCity(false);
+            } if(editCity === false) {
+              setEditCity(true);
+            }
+            // setEditCity((proslaVrednost) => !proslaVrednost)
+          }}>Izmeni grad</button>
+          {editCity === true && <>
+          <input type="text" className="input" onChange={updateCity}/>
+          <button className="button-succes" onClick={() => {updateUserOnBackend()}}>Primeni</button>
+          </> }
+          
             
           <button className="operation" onClick={() => {
             setNumber(number + 1);
-            console.log('POvecano na:', number + 1);
             }}>+</button>
           <h1>{number}</h1>
           <button className="operation" onClick={() => setNumber(number - 1)}>-</button>
-          <button className="btn" onClick={logOut}>Izloguj se</button>
+          <button className="button-danger" onClick={logOut}>Izloguj se</button>
           
         </div>
     ) 
-    }
+    
   }
 
-
-  const userInput = (event) => {
-    let loginValue = event.target.value;
-    setUserName(loginValue);
-    localStorage.setItem('userName',loginValue);
-    //proveri jel trebaju ova 2  lokal storidza
-  }
-  const passwordInput = (event) => {
-    let passwordValue = event.target.value;
-    setPassword(passwordValue);
-    localStorage.setItem('password',passwordValue);
-  }
+ 
  
 
   if(page === "login") {
@@ -137,9 +141,9 @@ const updateUserOnBackend = async () => {
           <input type="text" value={userName} onChange={userInput}></input>
        </div> */}
         <label htmlFor="username">Unesite vase ime:  </label>
-        <input type="text" value={userName} onChange={userInput} class="input"></input>
+        <input type="text" value={userName} onChange={userInput} className="input"></input>
         <label htmlFor="password">Unesite vasu lozinku:  </label> 
-        <input type="password" value={password} onChange={passwordInput} class="input"></input>
+        <input type="password" value={password} onChange={passwordInput} className="input"></input>
        {/* <div className="form-group" >
           <label htmlFor="">Lozinka:  </label>
           <input type="password" className="password-input" value={password} onChange={passwordInput}></input>
@@ -155,17 +159,16 @@ return (
        <div className="form-group" >
         <h1>Dobro dosli, molim registrujte se </h1>
         <label htmlFor="username">Unesite vase ime:  </label>
-        <input type="text" value={userName} onChange={userInput} class="input"></input>
+        <input type="text" value={userName} onChange={userInput} className="input"></input>
         {/* <input type="text" value={userName} onChange={userInput} /> */}
         </div>
        <div className="form-group" >
         <label htmlFor="password">Unesite vasu lozinku:  </label>
         {/* <input type="password" className="password-input" value={password} onChange={passwordInput} /> */}
-        <input type="password" value={password} onChange={passwordInput} class="input"></input>
+        <input type="password" value={password} onChange={passwordInput} className="input"></input>
       </div>
       <button className="button-succes" onClick={() => {
         register()
-        setPage("login")
         }}> Register </button>
         <button className="button-danger" onClick={() => {setPage("login")}}>Back to login</button>
     </div>
